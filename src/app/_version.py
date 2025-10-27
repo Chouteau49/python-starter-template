4434dcd963cfa37bf8b8da6aa97fae6edcca01da
julien <chouteau49@gmail.com>
2025-10-27 23:32:16 +0100

    chore(release): bump version to 0.1.3

--- src/app/_version.py
+++ src/app/_version.py
@@ -25,7 +25,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.2.dev3+g7203b0f21.d20251027"
-__version_tuple__ = version_tuple = (0, 1, 2, "dev3", "g7203b0f21.d20251027")
+__version__ = version = "0.1.3"
+__version_tuple__ = version_tuple = (0, 1, 3)
 
-__commit_id__ = commit_id = "g7203b0f21"
+__commit_id__ = commit_id = None
